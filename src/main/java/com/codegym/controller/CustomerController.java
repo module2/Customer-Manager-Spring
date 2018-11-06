1262832e25751df3061d2a5a0e90f6220e16e720
SON NGUYEN <myantj3m@gmail.com>
2018-11-06 09:13:08 +0700

    refactor code

--- src/main/java/com/codegym/controller/CustomerController.java
+++ src/main/java/com/codegym/controller/CustomerController.java
@@ -6,6 +6,9 @@ import com.codegym.service.CustomerServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class CustomerController {
@@ -21,5 +24,45 @@ public class CustomerController {
     public String create(Model model) {
         model.addAttribute("customer", new Customer());
         return "create";
+    }
+
+    @GetMapping("/customer/{id}/delete")
+    public String delete(@PathVariable int id, Model model) {
+        model.addAttribute("customer", customerService.findById(id));
+        return "delete";
+    }
+
+    @GetMapping("/customer/{id}/view")
+    public String view(@PathVariable int id, Model model) {
+        model.addAttribute("customer", customerService.findById(id));
+        return "view";
+    }
+
+    @GetMapping("/customer/{id}/edit")
+    public String edit(@PathVariable int id, Model model) {
+        model.addAttribute("customer", customerService.findById(id));
+        return "edit";
+    }
+
+    @PostMapping("/customer/update")
+    public String update(Customer customer, RedirectAttributes redirect) {
+        customerService.update(customer.getId(), customer);
+        redirect.addFlashAttribute("success", "Modified customer successfully!");
+        return "redirect:/";
+    }
+
+    @PostMapping("/customer/save")
+    public String save(Customer customer, RedirectAttributes redirect) {
+        customer.setId((int)(Math.random() * 10000));
+        customerService.save(customer);
+        redirect.addFlashAttribute("success", "Saved customer successfully!");
+        return "redirect:/";
+    }
+
+    @PostMapping("/customer/delete")
+    public String remove(Customer customer, RedirectAttributes redirect) {
+        customerService.remove(customer.getId());
+        redirect.addFlashAttribute("success", "Deleted customer successfully!");
+        return "redirect:/";
     }
 }
--- src/main/java/com/codegym/service/CustomerServiceImpl.java
+++ src/main/java/com/codegym/service/CustomerServiceImpl.java
@@ -8,42 +8,42 @@ import java.util.List;
 import java.util.Map;
 
 public class CustomerServiceImpl implements CustomerService {
-    private static Map<Integer, Customer> customerMap;
+    private static Map<Integer, Customer> customers;
 
     static {
-        customerMap = new HashMap<>();
-        customerMap.put(1, new Customer(1, "Truong", "truong@codegym.vn", "Hanoi"));
-        customerMap.put(2, new Customer(2, "Tien", "tnt@codegym.vn", "Thaibinh"));
-        customerMap.put(3, new Customer(3, "Dat", "dat@codegym.vn", "Thaibinh"));
-        customerMap.put(4, new Customer(4, "Long", "tieulongnu@codegym.vn", "Laocai"));
-        customerMap.put(5, new Customer(5, "Hau", "hauss@codegym.vn", "Hanoi"));
-        customerMap.put(6, new Customer(6, "TuanAnh", "sasa@codegym.vn", "Ninhbinh"));
+        customers = new HashMap<>();
+        customers.put(1, new Customer(1, "Truong", "truong@codegym.vn", "Hanoi"));
+        customers.put(2, new Customer(2, "Tien", "tnt@codegym.vn", "Thaibinh"));
+        customers.put(3, new Customer(3, "Dat", "dat@codegym.vn", "Thaibinh"));
+        customers.put(4, new Customer(4, "Long", "tieulongnu@codegym.vn", "Laocai"));
+        customers.put(5, new Customer(5, "Hau", "hauss@codegym.vn", "Hanoi"));
+        customers.put(6, new Customer(6, "TuanAnh", "sasa@codegym.vn", "Ninhbinh"));
     }
 
     @Override
     public List<Customer> findAll(){
-        return new ArrayList<>(customerMap.values());
+        return new ArrayList<>(customers.values());
     }
 
     @Override
     public void save(Customer customer){
 
-        customerMap.put(customer.getId(),customer);
+        customers.put(customer.getId(),customer);
     }
 
     @Override
     public Customer findById(int id){
-        return customerMap.get(id);
+        return customers.get(id);
     }
 
     @Override
     public  void update(int id, Customer customer){
-        customerMap.put(id, customer);
+        customers.put(id, customer);
 
     }
 
     @Override
     public void remove(int id){
-        customerMap.remove(id);
+        customers.remove(id);
     }
 }
